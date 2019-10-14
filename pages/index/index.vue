bd927b63bf71b49c941549d81f0bcd1b8175532d
jiangqianghua <240437339@qq.com>
2019-10-15 00:16:20 +0800

    完成轮播图后台获取数据

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,9 @@
 <template>
 	<view class="page">
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" class="carousel">
-			<swiper-item>
+			<swiper-item v-for="(item, index) in carouselList" :key="index">
 				<view class="swiper-item">
-					<image src="../../static/carousel/batmanvssuperman.png" mode="" class="carousel"></image>
-				</view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item">
-					<image src="../../static/carousel/spiderman.png" mode="" class="carousel"></image>
+					<image :src="item.image" mode="" class="carousel"></image>
 				</view>
 			</swiper-item>
 		</swiper>
@@ -19,11 +14,18 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				carouselList: []
 			}
 		},
 		onLoad() {
-
+			uni.request({
+				url: 'http://192.168.1.101:8000/index/carousel/list.json', //仅为示例，并非真实接口地址。
+				success: (res) => {
+					if (res.data.status == 200){
+						this.carouselList = res.data.data
+					}
+				}
+			});
 		},
 		methods: {
 
